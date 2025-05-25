14cd4270acb67ba5d5f54e6322ed51a52cfa280e
YJ <overdosedizzy@gmail.com>
2025-05-24 21:03:40 -0700

    added time converter, completed experiment handle process

--- backend/app/handlers/experiment_handler.py
+++ backend/app/handlers/experiment_handler.py
@@ -1,7 +1,9 @@
 from mlflow import MlflowClient
-
 # all the mlflow variables are well defined, use get() and it reads env or default
 from mlflow.environment_variables import MLFLOW_TRACKING_URI
+
+from backend.app.schemas.experiment_response import ExperimentResponse
+from backend.app.utils.time_convert import convert
 
 
 class ExperimentHandler:
@@ -9,11 +11,22 @@ class ExperimentHandler:
         self.__client = MlflowClient(MLFLOW_TRACKING_URI.get())
 
     def get_experiment_list(self):
-        return self.__client.search_experiments()
+        exp_ls = self.__client.search_experiments()
+
+        # wrap up with basemodel for fastapi
+        return [
+            ExperimentResponse(
+                id=exp.experiment_id,
+                name=exp.name,
+                artifact_location=exp.artifact_location,
+                lifecycle_stage=exp.lifecycle_stage,
+                tags=exp.tags,
+                creation_time=convert(exp.creation_time),
+                last_update_time=convert(exp.last_update_time)
+            )
+            for exp in exp_ls
+        ]
 
     def get_experiment_by_id(self):
         # TODO: get exp by id
         pass
-
-
-
---
+++ backend/app/utils/time_convert.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timezone, timedelta
+
+
+def convert(timestamp):
+    # PST = UTC-8
+    pst = timezone(timedelta(hours=-8))
+
+    # Convert to datetime, mind millisecond
+    dt = datetime.fromtimestamp(timestamp / 1000, tz=pst)
+    print(dt)
+    # ISO format
+    iso_str = dt.isoformat()
+
+    return iso_str
+
+
