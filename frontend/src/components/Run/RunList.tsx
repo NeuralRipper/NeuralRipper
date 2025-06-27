640fe074c08ecd317ceab18a69cdacc23cce0943
YJ <overdosedizzy@gmail.com>
2025-06-27 01:24:56 -0700

    remove loading status to avoid sidebar collapse

--- frontend/src/components/Run/RunList.tsx
+++ frontend/src/components/Run/RunList.tsx
@@ -5,13 +5,11 @@ import type {RunListProps, Run} from '../types/types.ts';
 
 const RunList: React.FC<RunListProps> = ({experimentId, onSelectedRunId}) => {
     const [runs, setRuns] = useState<Run[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         // only use for this useEffect hook
         const getRuns = async () => {
             try {
-                setLoading(true);
                 const url = `http://localhost:8000/runs/list/${experimentId}`;
                 const response = await fetch(url);
 
@@ -24,21 +22,11 @@ const RunList: React.FC<RunListProps> = ({experimentId, onSelectedRunId}) => {
                 setRuns(data);
             } catch (error) {
                 console.error('Error fetching runs:', error);
-            } finally {
-                setLoading(false);
             }
         };
 
         getRuns();
     }, [experimentId]);
-
-    if (loading) {
-        return (
-            <div className="ml-4 text-gray-400 text-xs font-mono">
-                Loading runs...
-            </div>
-        );
-    }
 
     if (runs.length === 0) {
         return (
