9cb3728ae30c40833e35a518568dc48dfae7ff7c
YJ <overdosedizzy@gmail.com>
2025-07-24 02:15:14 -0700

    1. Terminal component added. 2. Terminal model prompt streaming backend logic added, with handler and router, with lifespan for model loading. 3. TODO: slower streaming speed, model loading still fails.

--- backend/app/app.py
+++ backend/app/app.py
@@ -3,12 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
 
 from app.routers import experiment_router, run_router
+from app.routers import infer_router
+from app.routers.infer_router import lifespan
 
 # orjson.dumps() 4x Faster serialization of JSON
 # at the global level compared to json.dumps()
-app = FastAPI(default_response_class=ORJSONResponse)
+app = FastAPI(default_response_class=ORJSONResponse, lifespan=lifespan)
 app.include_router(experiment_router.router)
 app.include_router(run_router.router)
+app.include_router(infer_router.router)
 
 # Mind, different port is also different origin
 origins = [
