5ab90724b77d71a95e5ba961a90ace510b0a5da4
YJ <overdosedizzy@gmail.com>
2025-10-05 19:37:24 -0700

    Remove llm eval and prime intellect files, ready to redo all of them. Remove settings.py which we don't use anymore

--- backend/app/utils/share_mlflow_client.py
+++ backend/app/utils/share_mlflow_client.py
@@ -1,10 +1,11 @@
+import os
 from mlflow import MlflowClient
-from settings import MLFLOW_TRACKING_URI
 
 _mlflow_client = None
 
 def get_mlflow_client():
     global _mlflow_client
     if _mlflow_client is None:
-        _mlflow_client = MlflowClient(MLFLOW_TRACKING_URI)
+        tracking_uri = os.getenv('MLFLOW_TRACKING_URI', 'http://localhost:5000')
+        _mlflow_client = MlflowClient(tracking_uri)
     return _mlflow_client
--- backend/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Read from environment variable, fallback to default, set by docker-compose.yml
-MLFLOW_TRACKING_URI = os.getenv('MLFLOW_TRACKING_URI', 'http://localhost:5000')
-
-print(f"Backend using MLflow URI: {MLFLOW_TRACKING_URI}")
